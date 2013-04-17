850be73ebab2f7db16bcacadf4e49f807e635095
Ryan Urabe <rurabe@gmailcom>
2013-04-17 14:25:15 -0700

    rails and puma

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sloanconnect::Application.config.secret_token = 'acd290158c9edb5e34225431b88d0c3ac844a85a99d7c1e841ab6dc6a1eb3a26dc70f89734d228d77004b7351ef367ea57d2ed37339183a89e569c6146320734'
